d36c7e6ff72dba4097d9fae8069e679dbcead19c
nikhilpooja <nik703.v@gmail.com>
2018-05-24 19:45:46 +0530

    24 may

--- fundoonote/src/main/java/com/fundoonotes/noteservice/INoteService.java
+++ fundoonote/src/main/java/com/fundoonotes/noteservice/INoteService.java
@@ -6,10 +6,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface INoteService {
 
-	int createNote(Note note);
+	int createNote(Note note, int id);
 	int updateNote(Note note);
 	void deleteNote(int id);
-	void getNotes(int id);	
+	Note getNotes(int id);	
 	void saveImage(MultipartFile fileUpload, int noteId) throws IOException;
-	
+	Note getNoteByNoteId(int id);
 }
--- fundoonote/src/main/java/com/fundoonotes/noteservice/Note.java
+++ fundoonote/src/main/java/com/fundoonotes/noteservice/Note.java
@@ -5,11 +5,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fundoonotes.userservice.User;
 
 @Entity
 @Table(name="Note")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Note {
 	
 	@Id
@@ -34,6 +40,11 @@ public class Note {
 	@Lob
 	@Column
 	private byte[]image;
+	
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
+
 
 	public int getId() {
 		return id;
@@ -89,6 +100,14 @@ public class Note {
 
 	public void setImage(byte[] image) {
 		this.image = image;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
 	

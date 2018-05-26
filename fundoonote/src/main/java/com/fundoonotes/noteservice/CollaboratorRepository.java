c1d93f782c94fc19d62a6095915974c55ad7145f
SanaShaikh28 <SanaShaikh28>
2018-05-26 12:23:32 +0530

    rsolved conflicts

--- fundoonote/src/main/java/com/fundoonotes/noteservice/CollaboratorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fundoonotes.noteservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository(value="collaboratorRepository")
-public interface CollaboratorRepository extends JpaRepository<Collaborator, Integer> {
-
-}
--- fundoonote/src/main/java/com/fundoonotes/noteservice/ICollaboratorService.java
+++
@@ -1,7 +0,0 @@
-package com.fundoonotes.noteservice;
-
-public interface ICollaboratorService {
-
-	void createCollaborator(String sharedUseremail, int noteId, int userId);
-	void removeCollaborator(String sharedUseremail, int noteId);
-}

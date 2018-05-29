ad8637c70ab4544458a96374fd55db73fb0762ed
SanaShaikh28 <SanaShaikh28>
2018-05-29 14:59:58 +0530

    Modify collaborator service

--- fundoonote/src/main/java/com/fundoonotes/noteservice/ICollaboratorService.java
+++ fundoonote/src/main/java/com/fundoonotes/noteservice/ICollaboratorService.java
@@ -2,8 +2,9 @@ package com.fundoonotes.noteservice;
 
 public interface ICollaboratorService {
 
-	void createCollaborator(String sharedUseremail, int noteId, int userId);
+//	void createCollaborator(String sharedUseremail, int noteId, int userId);
+   
+   void createCollaborator(CollaboratorDto collaboratorDto, int id);
 
 	void removeCollaborator(String sharedUseremail, int noteId);
-
 }

04dcb66f93b19fc4ed72ac77809ee29cd8f375ec
SanaShaikh28 <SanaShaikh28>
2018-06-04 12:26:20 +0530

    modified admin controller

--- fundoonote/src/main/java/com/fundoonotes/adminservice/AdminRepositry.java
+++ fundoonote/src/main/java/com/fundoonotes/adminservice/AdminRepositry.java
@@ -1,20 +1,11 @@
 package com.fundoonotes.adminservice;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fundoonotes.userservice.User;
 
-public interface AdminRepositry extends JpaRepository<User,Integer>{
-	
-	User findByEmail(String email);
-	 //get note particular user
-	 // long getNoteCount(User user);
-	  //get number notes which contain images
-	  // long getImageNoteCount(User user);
-	   
-	   
+public interface AdminRepositry extends JpaRepository<User, Integer>
+{
 
-	
+   User findByEmail(String email);
 }

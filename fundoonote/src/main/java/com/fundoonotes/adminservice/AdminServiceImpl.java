f0865d81d94f8359433e8a39e73d7ceec4adf0ee
SanaShaikh28 <SanaShaikh28>
2018-06-04 12:26:48 +0530

    modified admin service

--- fundoonote/src/main/java/com/fundoonotes/adminservice/AdminServiceImpl.java
+++ fundoonote/src/main/java/com/fundoonotes/adminservice/AdminServiceImpl.java
@@ -1,7 +1,6 @@
 package com.fundoonotes.adminservice;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -9,47 +8,53 @@ import javax.transaction.Transactional;
 import org.apache.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.fundoonotes.noteservice.NoteRepository;
 import com.fundoonotes.userservice.User;
-import com.fundoonotes.userservice.UserDto;
-
-import com.fundoonotes.utility.TokenUtils;
 import com.fundoonotes.utility.UserNotedDto;
 
 @Service
-public class AdminServiceImpl implements IAdminService {
-	@Autowired
-	AdminRepositry adminRepositry;
-	@Autowired
-	NoteRepository noteRepository;
-	private static final Logger logger = Logger.getLogger(AdminServiceImpl.class);
+public class AdminServiceImpl implements IAdminService
+{
 
-	@Transactional
-	@Override
-	public List<UserNotedDto> getUserNoteCount() {
-		List<UserNotedDto> userNoteCountList = new ArrayList<UserNotedDto>();
-		
-		Iterable<User> users = adminRepositry.findAll();
-		for (User user : users) {
-			System.out.println(user.getUserId());
-		
-			long noteCount =noteRepository.countByUser(user);
-			System.out.println("total note of user==>"+noteCount);
-			
-	       long imageNoteCount = noteRepository.getImageNoteCount(user);
-			System.out.println(" total imageNoteCount==>"+imageNoteCount);
-			        
-			UserNotedDto userNoteDto = new UserNotedDto(user);
-			userNoteDto.setNoteCount(noteCount);
-			userNoteDto.setImageNoteCount(imageNoteCount);
-			userNoteDto.setTextNoteCount(noteCount-imageNoteCount);
-			System.out.println(" total textNoteCount==>"+userNoteDto.getTextNoteCount());
-			userNoteCountList.add(userNoteDto);
-		}
-		return userNoteCountList;
-	}
+   @Autowired
+   AdminRepositry adminRepositry;
+
+   @Autowired
+   NoteRepository noteRepository;
+   private static final Logger logger = Logger.getLogger(AdminServiceImpl.class);
+   
+   @Override
+   public void login(AdminDto adminDto)
+   {
+//      if(role==admin)
+   }
+   
+   @Transactional
+   @Override
+   public List<UserNotedDto> getUserNoteCount()
+   {
+      List<UserNotedDto> userNoteCountList = new ArrayList<UserNotedDto>();
+
+      Iterable<User> users = adminRepositry.findAll();
+      for (User user : users) {
+         System.out.println(user.getUserId());
+
+         long noteCount = noteRepository.countByUser(user);
+         System.out.println("total note of user==>" + noteCount);
+
+         long imageNoteCount = noteRepository.getImageNoteCount(user);
+         System.out.println(" total imageNoteCount==>" + imageNoteCount);
+
+         UserNotedDto userNoteDto = new UserNotedDto(user);
+         userNoteDto.setNoteCount(noteCount);
+         userNoteDto.setImageNoteCount(imageNoteCount);
+         userNoteDto.setTextNoteCount(noteCount - imageNoteCount);
+         System.out.println(" total textNoteCount==>" + userNoteDto.getTextNoteCount());
+         userNoteCountList.add(userNoteDto);
+      }
+      return userNoteCountList;
+   }
 
 }
